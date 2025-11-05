99a1b04c7d439b3a0a5c6440dd0aa94ce1f66e5f
a3 <xyz@mail.com>
2025-11-05 11:31:52 +0300

    add func for webstudio

--- src/api/lib/cors.ts
+++ src/api/lib/cors.ts
@@ -2,18 +2,26 @@
 import { cors } from "hono/cors";
 
 export const corsMiddleware = cors({
-  origin: [
-    "https://301.st",
-    "https://app.301.st",
-    "https://api.301.st",     // основное API
-    "https://dev.301.st",     // тестовый фронтенд
-    "http://localhost:8787",  // локальные тесты
-    "http://127.0.0.1:8787",  // альтернатива
-  ],
+  origin: (origin) => {
+    if (!origin) return false;
+
+    const allowed = [
+      "https://301.st",
+      "https://app.301.st",
+      "https://api.301.st",     // основное API
+      "https://dev.301.st",     // тестовый фронтенд
+      "http://localhost:8787",  // локальные тесты
+      "http://127.0.0.1:8787",  // альтернатива
+    ];
+
+    //  *.webstudio.is
+    if (origin.endsWith(".webstudio.is")) return true;
+
+    return allowed.includes(origin);
+  },
   allowHeaders: ["Accept", "Content-Type", "Authorization"],
   allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   maxAge: 86400,
 });
-
 
