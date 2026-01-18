6bab1f8ff65f75eea76c63ffa4037b41ba5499f5
a3 <xyz@mail.com>
2026-01-18 17:19:36 +0300

    add route

--- src/api/integrations/index.ts
+++ src/api/integrations/index.ts
@@ -10,10 +10,16 @@ export * from "./keys/storage";
 // Cloudflare
 export { handleInitKeyCF } from "./providers/cloudflare/initkey";
 export * from "./providers/cloudflare/permissions";
+export * as CFWorkers from "./providers/cloudflare/workers";
+export * as CFD1 from "./providers/cloudflare/d1";
 
 // Namecheap
 export { handleInitKeyNamecheap } from "./providers/namecheap/initkey";
 export * as Namecheap from "./providers/namecheap/namecheap";
+
+// VirusTotal
+export { handleInitKeyVirusTotal, handleGetVirusTotalQuota } from "./providers/virustotal/initkey";
+export * as VirusTotal from "./providers/virustotal/initkey";
 
 // Адаптеры (по необходимости)
 export * as Namesilo from "./providers/namesilo/namesilo";
